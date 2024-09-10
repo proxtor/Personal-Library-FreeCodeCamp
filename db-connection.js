d10af16c4f6a64bab9b0e3373f4e9685ff1b6980
proxtor <75043312+proxtor@users.noreply.github.com>
2024-09-10 14:02:12 -0700

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false, // Добавьте это
+});
+
 const db = mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
